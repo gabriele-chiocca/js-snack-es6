3c915c612df6fc781d8c572f88bd0dd92311a938
gabriele-chiocca <gabrielechiocca00@gmail.com>
2026-01-16 13:39:26 +0100

    bikes array

---
+++ Snack1/js/script.js
@@ -0,0 +1,14 @@
+//Bonus farle con filter, map, reduce
+
+const bikes = [
+  { nome: 'Montains', peso: 290 },
+  { nome: 'Gioins', peso: 110 },
+  { nome: 'Sarti', peso: 90 },
+  { nome: 'Clinsi', peso: 170 },
+  { nome: 'Gioice', peso: 320 },
+  { nome: 'Crocs', peso: 280 },
+  { nome: 'Crains', peso: 130 },
+  { nome: 'Scisyl', peso: 160 },
+];
+
+let biciminore = bikes[0].peso;
